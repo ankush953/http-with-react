1c4f6a72795e5503c1bd78dc6f20a22c8d92b5b6
ankush953 <ankushraghuvanshi07@gmail.com>
2020-07-26 15:33:56 +0530

    called dummyAPI and put it on screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,16 +4,26 @@ import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import classes from "./Blog.module.css";
+import Axios from "axios";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    Axios.get("https://jsonplaceholder.typicode.com/posts").then((response) =>
+      this.setState({ posts: response.data })
+    );
+  }
+
   render() {
+    const posts = this.state.posts.map((post) => (
+      <Post key={post.id} title={post.title} />
+    ));
     return (
       <div>
-        <section className={classes.Posts}>
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className={classes.Posts}>{posts}</section>
         <section>
           <FullPost />
         </section>
