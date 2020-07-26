6c5d94b1e744845a2560ce769e2e23b410d9892b
ankush953 <ankushraghuvanshi07@gmail.com>
2020-07-26 15:44:19 +0530

    limited post to 4, added author hardcoded

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -12,14 +12,21 @@ class Blog extends Component {
   };
 
   componentDidMount() {
-    Axios.get("https://jsonplaceholder.typicode.com/posts").then((response) =>
-      this.setState({ posts: response.data })
-    );
+    Axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "ankush",
+        };
+      });
+      this.setState({ posts: updatedPosts });
+    });
   }
 
   render() {
     const posts = this.state.posts.map((post) => (
-      <Post key={post.id} title={post.title} />
+      <Post key={post.id} title={post.title} author={post.author} />
     ));
     return (
       <div>
