92c37e27dd6d76ad81bfc4d8d9de6dce721cd2d0
ankush953 <ankushraghuvanshi07@gmail.com>
2020-07-27 13:05:11 +0530

    handling error while calling API

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,19 +10,22 @@ class Blog extends Component {
   state = {
     posts: [],
     selectedPostId: null,
+    error: false,
   };
 
   componentDidMount() {
-    Axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map((post) => {
-        return {
-          ...post,
-          author: "ankush",
-        };
-      });
-      this.setState({ posts: updatedPosts });
-    });
+    Axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "ankush",
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(() => this.setState({ error: true }));
   }
 
   selectedPostHandler = (id) => {
@@ -30,14 +33,17 @@ class Blog extends Component {
   };
 
   render() {
-    const posts = this.state.posts.map((post) => (
-      <Post
-        key={post.id}
-        title={post.title}
-        author={post.author}
-        clicked={() => this.selectedPostHandler(post.id)}
-      />
-    ));
+    let posts = <p>Something went wrong!!!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.selectedPostHandler(post.id)}
+        />
+      ));
+    }
     return (
       <div>
         <section className={classes.Posts}>{posts}</section>
